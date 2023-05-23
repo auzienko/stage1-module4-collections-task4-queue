586f981bb9f2c97d974eaa0f6d6ad10763dda5c4
Aleksandr Uzienko <a.uzienko@gmail.com>
2023-05-23 17:55:29 +0300

    solution

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,34 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+        toDesk(arrayDeque, firstQueue);
+        toDesk(arrayDeque, firstQueue);
+        toDesk(arrayDeque, secondQueue);
+        toDesk(arrayDeque, secondQueue);
+
+        while (!(firstQueue.isEmpty() && secondQueue.isEmpty())) {
+            fromDesk(arrayDeque, firstQueue);
+            toDesk(arrayDeque, firstQueue);
+            toDesk(arrayDeque, firstQueue);
+            fromDesk(arrayDeque, secondQueue);
+            toDesk(arrayDeque, secondQueue);
+            toDesk(arrayDeque, secondQueue);
+        }
+        return arrayDeque;
+    }
+
+    private void toDesk(ArrayDeque<Integer> arrayDeque, Queue<Integer> playerQueue) {
+        if (playerQueue.isEmpty()) {
+            return;
+        }
+        arrayDeque.add(playerQueue.poll());
+    }
+
+    private void fromDesk(ArrayDeque<Integer> arrayDeque, Queue<Integer> playerQueue) {
+        if (playerQueue.isEmpty()) {
+            return;
+        }
+        playerQueue.add(arrayDeque.pollLast());
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,23 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        Deque<Integer> dishes = new LinkedList<>();
+        for (int i = 1; i <= numberOfDishes; ++i) {
+            dishes.add(i);
+        }
+        int steps = 0;
+        List<Integer> resultList = new ArrayList<>();
+        while (!dishes.isEmpty()) {
+            if (++steps % everyDishNumberToEat == 0) {
+                resultList.add(dishes.pollFirst());
+            } else {
+                Integer j = dishes.pollFirst();
+                dishes.addLast(j);
+            }
+        }
+        return resultList;
     }
 }
